77b2dda0aef1bbb1d6dd3f1938ee79916bd2c8e7
Renan Arimatea <renanmoraiz@gmail.com>
2021-11-08 20:38:14 -0300

    Trabalhando com conceitos básicos de variáveis em JavaScript.

---
+++ variaveis/index.js
@@ -0,0 +1,60 @@
+// Exemplo de Comentários
+
+// comenta uma linha
+
+/* Comenta
+um
+Bloco */
+
+
+// Exemplo de console.log
+
+console.log("Hello World!")
+
+console.log("Tudo o que estiver dentro do console.log vai aparecer para os desenvolvedores!")
+
+
+//Exemplo de prompt
+
+/* const nome = prompt("Qual o seu nome?")
+console.log(nome) */
+
+
+// Exemplo de declaração de variáveis
+
+const idade = 89
+console.log(idade)
+
+
+let idade2 = 23
+idade2 = 26
+console.log(idade2)
+
+const nomeCompleto = "Renan Arimatea"
+
+console.log("Olá! Meu nome é",  nomeCompleto, "e tenho", idade2, "anos.")
+
+const minhaIdade = 20
+const idadeDaJu = 19
+
+const soma = minhaIdade + idadeDaJu
+
+console.log(soma)
+
+
+let booleano = true
+booleano =  false
+console.log(booleano)
+
+
+// PRATICANDO
+
+const meuNome = "Renan"
+const meuSobrenome = "Arimatea"
+let minhaIdade2 = 26
+let statusAtual = true
+
+console.log("Olá, meu nome é", meuNome, meuSobrenome, "e eu tenho", minhaIdade, "anos.")
+
+console.log(meuNome, meuSobrenome, "é um estudante?", statusAtual)
+
