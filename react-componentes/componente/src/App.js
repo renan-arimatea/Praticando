58643efea4c0c402405d1a04dfde0fa2c2e91d79
Renan Arimatea <renanmoraiz@gmail.com>
2022-01-26 20:25:13 -0300

    Criado primeiro componente em React.

---
+++ react-componentes/componente/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react"
+import MeuPrimeiroComponente from './components/MeuPrimeiroComponente'
+
+export default function App() {
+
+  return (
+
+    <div className="App">
+      <MeuPrimeiroComponente/>
+    </div>
+
+  );
+}
---
+++ react-componentes/componente/src/components/MeuPrimeiroComponente.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const MeuPrimeiroComponente = () => {
+    return (
+        <div>
+            <h1>Caraca que componente IRADO!</h1>
+            <p>Isso é muito Black Mirror</p>
+        </div>
+    )
+}
+
+export default MeuPrimeiroComponente
