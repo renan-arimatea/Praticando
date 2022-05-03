48b7e1bbef9cd03e6777136882b50ba15c78d0ac
Renan Arimatea <renanmoraiz@gmail.com>
2022-05-03 18:29:48 -0300

    Criado as páginas da aplicação>

---
+++ react-cookenu/cookenu/src/pages/RecipesListPage/RecipesListPage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const RecipesListPage = () => {
+
+    return (
+        <div>
+            <h1>RecipeListPage</h1>
+        </div>
+    )
+}
+
+export default RecipesListPage
